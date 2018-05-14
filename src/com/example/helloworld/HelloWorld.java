915954cb6c2891cd8fea0ed3752707dc0bfd54b4
gacchaaaaann <TIBIkko.book@gmail.com>
2018-05-15 00:05:55 +0900

    1st

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hallo,World.");
+        System.out.println("Hallo,World.");
+    }
+}
+
+/** 実行結果 ***********
+ Hallo,World.
+ Hallo,World.
+
+ Process finished with exit code 0
+
+ **********************/
